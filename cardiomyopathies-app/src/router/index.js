ab8409cb1928bf943895e4378315daa7cf0a147e
alex-melia <alexmelia41@gmail.com>
2023-04-15 03:01:50 +0100

    Initialized project with firebase

---
+++ cardiomyopathies-app/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ cardiomyopathies-app/src/views/RegisterView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Register</h1>
+  </main>
+</template>
+
+<style></style>
