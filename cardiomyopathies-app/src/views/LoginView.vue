a885103ba4617b436fa8a71bc10111e28d00cacd
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-04-17 17:30:59 +0100

    'Tailwind''

--- cardiomyopathies-app/src/views/LoginView.vue
+++ cardiomyopathies-app/src/views/LoginView.vue
@@ -1,9 +1,11 @@
 <script setup></script>
 
 <template>
-  <main>
-    <h1>Login</h1>
-  </main>
+  <div class="bg-gray-200 p-4">
+    <h1 class="text-xl font-bold">Hello, Tailwind!</h1>
+    <p class="text-gray-600">This is a Vue component styled with Tailwind.</p>
+  </div>
 </template>
+
 
 <style></style>
