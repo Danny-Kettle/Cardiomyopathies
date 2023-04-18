7a25b49b546be498f684a48e22ab26ec2b8d7cbe
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-04-18 14:55:28 +0100

    Added Login and Signup functionality

--- cardiomyopathies-app/src/firebase/database.js
+++ cardiomyopathies-app/src/firebase/database.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 
 import {
   getAuth,
@@ -17,6 +18,7 @@ import {
   where,
   deleteDoc,
   getDocs,
+  doc,
   setDoc
 } from 'firebase/firestore'
 
@@ -31,6 +33,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 const firebaseAuthentication = getAuth()
 const firebaseFireStore = getFirestore()
 const timestamp = serverTimestamp()
@@ -52,5 +55,6 @@ export {
   where,
   deleteDoc,
   getDocs,
-  setDoc
+  setDoc,
+  doc
 }
--- cardiomyopathies-app/src/views/LoginView.vue
+++ cardiomyopathies-app/src/views/LoginView.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <div class="container mx-auto px-4 py-8">
@@ -6,7 +7,7 @@
           <div class="bg-white shadow-md rounded px-8 py-6">
             <h2 class="text-2xl font-semibold mb-4">{{ isSignup ? 'Create Account' : 'Login' }}</h2>
             <form @submit.prevent="handleSubmit">
-              <div class="mb-4" v-if="isSignup">
+              <div class="mb-4">
                 <label class="block mb-2 text-gray-800" for="email">Email</label>
                 <input
                   class="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
@@ -17,10 +18,10 @@
                   required
                 />
               </div>
-              <div class="mb-4">
+              <div class="mb-4" v-if="isSignup">
                 <label class="block mb-2 text-gray-800" for="username">Username</label>
                 <input
-                  class="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
+                  class="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400" 
                   type="text"
                   name="username"
                   id="username"
@@ -74,6 +75,17 @@
                   required
                 />
               </div>
+              <div class="mb-4" v-if="isSignup">
+                <label class="block mb-2 text-gray-800" for="institute">Institute</label>
+                <input
+                  class="w-full p-2 border border-gray-400 rounded-md focus:outline-none focus:border-blue-400"
+                  type="text"
+                  name="institute"
+                  id="institute"
+                  v-model="institute"
+                  required
+                />
+              </div>
               <div class="flex justify-between items-center">
                 <button
                   class="px-4 py-2 text-white rounded-md bg-blue-500 hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
@@ -84,7 +96,7 @@
                 <button
                   class="text-blue-500 hover:text-blue-600 focus:outline-none"
                   type="button"
-                  @click="toggleSignup"
+                  @click="isSignup = !isSignup"
                 >
                   {{
                     isSignup ? 'Already have an account? Log In' : "Don't have an account? Sign Up"
@@ -99,6 +111,9 @@
   </div>
 </template>
 <script>
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword , doc, firebaseAuthentication, firebaseFireStore , setDoc} from '../firebase/database'
+
+
 export default {
   data() {
     return {
@@ -118,28 +133,55 @@ export default {
         console.log(
           `Submitting sign up form with email: ${this.email}, username: ${this.username}, password: ${this.password}, confirm password: ${this.confirmPassword}, first name: ${this.firstName}, and last name: ${this.lastName}`
         )
-        // send request to sign up endpoint with user data
-
-        // show success alert
-        alert('Successfully signed up! You will now be redirected to the login page.')
-
-        // redirect to login page
-        this.$router.push('/login')
+        // create a new user in Firestore
+        createUserWithEmailAndPassword(firebaseAuthentication, this.email, this.password)
+          .then((userCredential) => {
+            const user = userCredential.user
+            const userData = {
+              email: this.email,
+              username: this.username,
+              firstName: this.firstName,
+              lastName: this.lastName,
+              institute: this.institute,
+              role: "user",
+            }
+            console.log("Firestore object:", firebaseFireStore);
+            console.log("User data:", userData);
+            return setDoc(doc(firebaseFireStore, 'users', user.uid), userData)
+          })
+          .then(() => {
+            // show success alert
+            alert('Successfully signed up! You will now be redirected to the login page.')
+            // redirect to login page
+            this.$router.push('/login')
+          })
+          .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            console.log(`Signup error: ${errorCode} - ${errorMessage}`)
+            // show error alert
+            alert(`Signup error: ${errorCode} - ${errorMessage}`)
+          })
       } else {
-        console.log(
+        console.log(  
           `Submitting login form with username: ${this.username} and password: ${this.password}`
         )
-        // send request to login endpoint with user data
-
-        // show success alert
-        alert('Successfully logged in!')
-
-        // redirect to dashboard page (replace with your desired page)
-        this.$router.push('/dashboard')
+        // sign in user with email and password in Firestore
+        signInWithEmailAndPassword(firebaseAuthentication, this.email, this.password)
+          .then(() => {
+            // show success alert
+            alert('Successfully logged in!')
+            // redirect to dashboard page (replace with your desired page)
+            this.$router.push('/')
+          })
+          .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            console.log(`Login error: ${errorCode} - ${errorMessage}`)
+            // show error alert
+            alert(`Login error: ${errorCode} - ${errorMessage}`)
+          })
       }
-    },
-    toggleSignup() {
-      this.isSignup = !this.isSignup
     }
   }
 }
