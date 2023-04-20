b5bd3dc0c986df3df7fbd341297b41b2f0414af5
ibrahim-007 <moughalibrahim@gmail.com>
2023-04-20 01:02:57 +0100

    new

--- cardiomyopathies-app/src/views/news.vue
+++ cardiomyopathies-app/src/views/news.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-    <h1>News Results</h1>
-    <ul>
-      <li v-for="article in articles" :key="article._id">
-        <h2>{{ article.source.Name }}</h2>
-        <h2>{{ article.title }}</h2>
-        <p>{{ article.summary }}</p>
-
-        <a :href="article.url">Read More</a>
-      </li>
-    </ul>
+  <div class="w-full py-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
+        <div v-for="(article, index) in articles" :key="index" class="relative h-96 rounded-lg cursor-pointer hover:opacity-80 transition-opacity text-white  overflow-hidden" @click="redirectToArticle(article.url)">
+          <img :src="imageUrls[index]" alt="" class="absolute inset-0 h-full w-full object-cover" />
+          <div class="absolute inset-0 bg-gradient-to-tl from-indigo-800 to-black opacity-60 z-10 "></div>
+          <div class="absolute flex flex-col items-start shadow-lg inset-0 z-20 p-4">
+            <h2 class="text-2xl text-center absolute font-bold mb-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{{ article.title }}</h2>
+            <div class="flex flex-row justify-between w-full mt-auto">
+              <p>{{ article.author && article.author.slice(0, 20) }}</p>  
+              <p>{{ article.publishedAt.slice(0, article.publishedAt.indexOf("T")) }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-
 export default {
   data() {
     return {
@@ -33,7 +37,6 @@ export default {
     .then(response => {
       this.articles = response.data.articles
       console.log(this.articles);
-
       // Map each article to a relevant image from Unsplash
       this.articles.forEach((article, index) => {
         axios.get('https://api.unsplash.com/search/photos', {
@@ -61,25 +64,7 @@ export default {
     }
   }
 }
-
 </script>
 
 <style scoped>
-h1 {
-  font-size: 24px;
-  margin-bottom: 20px;
-}
-
-
-li {
-  margin-bottom: 20px;
-}
-
-h2 {
-  font-size: 18px;
-  margin-bottom: 10px;
-}
-p {
-  margin-bottom: 10px;
-}
 </style>
