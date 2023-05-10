10b2376963487b0b2952ee195140893321b52612
alex-melia <alexmelia41@gmail.com>
2023-05-10 19:26:27 +0100

    added home page

--- cardiomyopathies-app/src/views/HomeView.vue
+++ cardiomyopathies-app/src/views/HomeView.vue
@@ -1,6 +1,41 @@
 <template>
-    Working
+  <h2
+    class="flex flex-col items-center justify-center font-bold text-3xl lg:text-4xl text-slate-600"
+  >
+    Cardiomyopathy Database
+  </h2>
+
+  <p class="mt-10 px-4 py-6 pt-0 ml-10 text-md lg:text-lg -mt-4 dark:text-gray-400">
+    This Web Application enables users, doctors, and students to view and query through a whole
+    Cardiomyopathy database in order to view cardiomyopathies and different gene mutations and other
+    symptoms that a patient may experience along with the ones that Cardiomyopathy presents.
+  </p>
+
+  <p class="px-4 mt-5 py-6 pt-0 ml-10 text-md lg:text-lg -mt-4 dark:text-gray-400">
+    Cardiomyopathy (kahr-dee-o-my-OP-uh-thee) is a condition affecting the heart muscle, which can
+    result in reduced blood-pumping ability and hinder the delivery of oxygen and nutrients to the
+    body's tissues. If left untreated, cardiomyopathy may progress to heart failure, a serious and
+    potentially life-threatening condition.
+  </p>
+
+  <p class="px-4 py-6 mt-5 pt-0 ml-10 text-md lg:text-lg -mt-4 dark:text-gray-400">
+    The main types of cardiomyopathy include dilated, hypertrophic, and restrictive cardiomyopathy.
+    Treatment — which might include medications, surgically implanted devices or, in severe cases, a
+    heart transplant — depends on which type of cardiomyopathy you have and how serious it is.
+  </p>
 </template>
 
 <script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
 </script>
+
+<style>
+p {
+  font-size: 18px;
+}
+</style>
