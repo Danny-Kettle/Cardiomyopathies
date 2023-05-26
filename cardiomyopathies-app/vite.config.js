6b7739f7389cae604ec2f14560a09c431baba248
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-05-26 18:18:16 +0100

    Assets Dir added to config

--- cardiomyopathies-app/vite.config.js
+++ cardiomyopathies-app/vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Cardiomyopathies/',
+  assetsDir: 'assets',
   assetPrefix: '/Cardiomyopathies/assets/',
   plugins: [vue()],
   resolve: {
